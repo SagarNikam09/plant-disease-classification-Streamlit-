3952e9ec5f0b0e3dc5c1b38d832569e27817caea
sagarnikam09 <sagarpatilx388@gmail.com>
2024-12-26 22:24:03 +0530

    add class names json file

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 import base64
+import json
+
 
 def model_prediction(test_image):
     model = tf.keras.models.load_model('trained_model.keras')
@@ -11,6 +13,12 @@ def model_prediction(test_image):
     input_arr = np.array([input_arr])  # Convert single image to a batch.
     predictions = model.predict(input_arr)
     return np.argmax(predictions, axis=1)[0]
+
+def load_class_names():
+    with open('class_names.json', 'r') as file:
+        data = json.load(file)
+    return data['class_names']
+
 
 def get_base64(file_path):
     """Function to encode file to base64"""
@@ -137,17 +145,5 @@ elif app_mode == "Disease Recognition":
             st.write("Our Prediction")
             result_index = model_prediction(test_image)
             # Reading Labels
-            class_name = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy',
-                          'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew',
-                          'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
-                          'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy',
-                          'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
-                          'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot',
-                          'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy',
-                          'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy',
-                          'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy',
-                          'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold',
-                          'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite',
-                          'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
-                          'Tomato___healthy']
+            class_name = load_class_names()
             st.write(class_name[result_index])
